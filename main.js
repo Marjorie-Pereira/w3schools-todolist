c5b986cc21c1e15f555db4f2c08e9fb2d8f8ba24
Marjorie-Pereira <marjorie300904@gmail.com>
2024-10-05 16:31:58 -0300

    close button function created to delete list items

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ for(i = 0; i < toDoList.length; i++) {
     span.appendChild(txt);
     toDoList[i].appendChild(span);
 }
+
+// hiding current list item when clicking close button
+var close = document.getElementsByClassName("close");
+var i;
+for(i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
