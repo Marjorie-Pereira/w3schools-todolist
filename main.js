e8576c043dc71353fc0f20781d6340e82763459c
Marjorie-Pereira <marjorie300904@gmail.com>
2024-10-08 15:36:19 -0300

    creating tasks function

--- main.js
+++ main.js
@@ -27,3 +27,31 @@ list.addEventListener('click', function(e) {
         e.target.classList.toggle('checked');
     }
 }, false);
+
+// create a new list item when clicking on the "Add" button
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("taskTitle").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("todoList").appendChild(li);
+    }
+    document.getElementById("taskTitle").value = "";
+
+    // close button
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for(i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
